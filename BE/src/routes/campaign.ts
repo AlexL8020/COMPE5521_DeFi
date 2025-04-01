2de376aa838eb3d431c8d306e2975949dcf7775d
fishkant <160023972+fishkant@users.noreply.github.com>
2025-04-02 04:42:51 +0800

    Add files via upload

--- BE/src/routes/campaign.ts
+++ BE/src/routes/campaign.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import {
   campaigndata,
+  getCampaigns,
+  getCampaignByID,
     // import other controllers as needed
   } from "../controllers/campaigndata";
 
 
 const router = Router();
 router.post("/", campaigndata);
-
+router.get("/", getCampaigns);
+router.get("/:id", getCampaignByID);
 
 export default router;
