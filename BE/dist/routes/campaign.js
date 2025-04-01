2919aae8968c73a1e9d9fea36520ad49e5113019
Alex Lam <23026113g@connet.polyu.hk>
2025-04-01 21:26:16 +0800

    Stop tracking BE/dist directory

--- BE/dist/routes/campaign.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const campaigndata_1 = require("../controllers/campaigndata");
-const router = (0, express_1.Router)();
-router.post("/", campaigndata_1.campaigndata);
-exports.default = router;
